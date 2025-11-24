2f4a5578120be9cdc98a3fec7f913c3d01448a70
José Vitor <jvpcampos13@gmail.com>
2025-11-24 00:43:42 -0300

    criação da main

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+*Autores: Iago Ribeiro Sales e José Vitor Pires Silveira Campos
+*Programa: ENUNCIADO 6 - BitCar
+*Data: 02/12/2025
+*/
+
+//bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <time.h>
+
+
+//estrutura
+
+//declaração de variáveis
+
+//função principal
+
+//funções
