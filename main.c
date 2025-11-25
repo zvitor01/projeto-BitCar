68df857115424f67e61a1d1db7e07fffd16a2021
José Vitor <jvpcampos13@gmail.com>
2025-11-25 00:28:11 -0300

    Criação da tela de carregamento e a tela de login

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <conio.h>
 #include <time.h>
+#include <windows.h>
 
 // Tipos especiais (enum)
 typedef enum {
@@ -38,16 +39,20 @@ struct cadastro
     char data[12];
 } carro;
 
-//declaração de variáveis
+//declaração de funções
 void gotoxy(int x, int y);
 void bordas();
+void telaCarregamento();
+void telaLogin();
+void menu();
 
 //função principal
 int main()
 {
     system("mode con:cols=80 lines=25");
-    bordas();
-    system("pause");
+
+    telaLogin();
+
     return 0;
 }
 
@@ -83,4 +88,99 @@ void bordas()
         printf("%c", 217); //canto inferior direito
     }
     printf("\n");
+}
+
+void telaCarregamento()
+{
+    system("cls");
+    bordas();
+
+    gotoxy(80/2 - 8, 5);
+    printf("PROJETO BITCAR"); //título centralizado
+
+    gotoxy(80/2 - 30, 7);
+    printf("Autores: Iago Ribeiro Sales e Jose Vitor Pires Silveira Campos"); //autores centralizados
+
+    gotoxy(80/2 - 10, 10);
+    printf("Carregando sistema..."); //texto de carregamento
+
+    int tam = 40;
+    int barraX = 80/2 - (tam/2); //barra centralizada
+
+    for (int i = 0; i <= tam; i++) //desenha a barra
+    {
+        gotoxy(barraX, 13);
+
+        printf("[");
+
+        for (int j = 0; j < i; j++) 
+            printf("%c", 219);
+
+        for (int j = i; j < tam; j++)
+            printf(" ");
+
+        printf("]");
+
+        gotoxy(80/2 - 3, 15);
+        printf("%d%%   ", (i * 100) / tam); //porcentagem
+
+        Sleep(80);
+    }
+
+    gotoxy(80/2 - 15, 18);
+    printf("Sistema carregado com sucesso!");
+
+    Sleep(900);
+}
+
+void telaLogin()
+{
+    char login[10] = "admin";
+    char senha[10] = "123";
+    char loginUser[10] = {0};
+    char senhaUser[10] = {0};
+
+    system("cls");
+	bordas();
+	gotoxy(28,2);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",218,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,191);
+	gotoxy(28,3);
+	printf("%c          BitCar          %c",179,179);
+    gotoxy(28,4);
+	printf("%c     Revenda de Carros    %c",179,179);
+	gotoxy(28,5);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",192,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,217);
+	gotoxy(35,6);
+	printf("Tela de Login");
+	gotoxy(28,7);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196);
+
+    gotoxy(28, 11);
+    printf ("Digite o usuario: ");
+    gotoxy(28, 12);
+    printf ("Digite a senha..: ");
+
+    gotoxy(46, 11);
+    fgets(loginUser, 10, stdin);
+    strtok(loginUser, "\n");
+    
+    gotoxy(46, 12);
+    fgets(senhaUser, 10, stdin);
+    strtok(senhaUser, "\n");
+
+    if(strcmp(login, loginUser) == 0 && strcmp(senha, senhaUser) == 0)
+        menu();
+    else
+    {   
+        gotoxy(25, 15);
+        printf("Usuario e/ou senha invalidos");
+        Sleep(2000);
+        telaLogin();
+    }
+}
+
+void menu()
+{
+    system("cls");
+    system("pause");
 }
